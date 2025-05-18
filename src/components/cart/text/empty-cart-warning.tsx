88cc5ef2c31e334fee7b60c4820d2877a913ff94
MarcsLeao <marcosleaouni@gmail.com>
2025-05-18 08:45:33 -0300

    fix: EmptyCartText component

---
+++ src/components/cart/text/empty-cart-warning.tsx
@@ -0,0 +1,7 @@
+export function EmptyCartText() {
+    return(
+        <div className="h-full flex justify-center items-center min-h-[250px]">
+            <p className="font-bold text-4xl text-zinc-500 text-center">Cart has no items</p>
+        </div>
+    )
+}
